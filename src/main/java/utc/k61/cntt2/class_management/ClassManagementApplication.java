e5e7d53a5fc339eabd6a7438a88ce395243fb56a
thuan-utc <thuansl99@gmail.com>
2024-02-13 20:27:27 +0700

    initial project

---
+++ src/main/java/utc/k61/cntt2/class_management/ClassManagementApplication.java
@@ -0,0 +1,13 @@
+package utc.k61.cntt2.class_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utc/k61/cntt2/class_management/ClassManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package utc.k61.cntt2.class_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
